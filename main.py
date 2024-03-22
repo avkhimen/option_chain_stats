fc8a24883a83f2003be9e93024245595be181aec
Ubuntu <Vadim Avkhimenia>
2024-03-22 06:50:54 +0000

    Adding loop to get prices

--- main.py
+++ main.py
@@ -3,37 +3,39 @@ from dotenv import load_dotenv
 import os
 import requests
 import pandas as pd
+import time
 
 load_dotenv()  # take environment variables from .env.
 
-token = os.getenv('marketdata_token')
-
-# Get stocks
-
-# Get option chain for each stock
-
-# Analyze the stock
-
-# The API endpoint for retrieving stock quotes for SPY
-url = 'https://api.marketdata.app/v1/stocks/quotes/SPY/'
-
-# Setting up the headers for authentication
-headers = {
-    'Accept': 'application/json',
-    'Authorization': f'Bearer {token}'
-}
-
-# Making the GET request to the API
-response = requests.get(url, headers=headers)
-
-# Checking if the request was successful
-if response. status_code in (200, 203):
-    # Parsing the JSON response
-    data = response.json()
-    print(data)
-else:
-    print(f'Failed to retrieve data: {response.status_code}')
-
 response = pd.read_json('company_tickers.json')
 
-print(response)
+tickers = []
+
+for k, v in response.items():
+    tickers.append(v['ticker'])
+
+token = os.getenv('marketdata_token')
+
+for ticker in tickers:
+
+    # The API endpoint for retrieving stock quotes for SPY
+    url = 'https://api.marketdata.app/v1/stocks/quotes/' + ticker + '/'
+
+    # Setting up the headers for authentication
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    # Making the GET request to the API
+    response = requests.get(url, headers=headers)
+
+    # Checking if the request was successful
+    if response. status_code in (200, 203):
+        # Parsing the JSON response
+        data = response.json()
+        print(data['ask'])
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+
+    time.sleep(0.1)
