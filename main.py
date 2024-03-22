d4db68433d62044d6696ef1c0b7fe87a1078bf9e
Ubuntu <Vadim Avkhimenia>
2024-03-22 07:15:00 +0000

    Adding code to get strikes

--- main.py
+++ main.py
@@ -34,7 +34,32 @@ for ticker in tickers:
     if response. status_code in (200, 203):
         # Parsing the JSON response
         data = response.json()
-        print(data['ask'])
+        ask = data['ask'][0]
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+
+    url = 'https://api.marketdata.app/v1/options/chain/' + ticker + '/'
+
+        # Setting up the headers for authentication
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    # Making the GET request to the API
+    response = requests.get(url, headers=headers)
+
+        # Checking if the request was successful
+    if response. status_code in (200, 203):
+        # Parsing the JSON response
+        data = response.json()
+        strikes = data['strike']
+        i = 0
+        for strike in strikes:
+            while strike <= ask:
+                i += 1
+        last_strike = strikes[i]
+        print(last_strike)
     else:
         print(f'Failed to retrieve data: {response.status_code}')
 
