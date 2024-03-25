2137978713b12c951893f152af964f0f08068d72
Ubuntu <Vadim Avkhimenia>
2024-03-25 20:11:58 +0000

    Printing ask

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for k, v in response.items():
 
 token = os.getenv('marketdata_token')
 
-for ticker in tickers:
+for ticker in tickers[:1]:
 
     # The API endpoint for retrieving stock quotes for SPY
     url = 'https://api.marketdata.app/v1/stocks/quotes/' + ticker + '/'
@@ -30,13 +30,17 @@ for ticker in tickers:
     # Making the GET request to the API
     response = requests.get(url, headers=headers)
 
+    print(response)
+
     # Checking if the request was successful
     if response. status_code in (200, 203):
         # Parsing the JSON response
         data = response.json()
-        ask = data['ask'][0]
+        ask = float(data['ask'][0])
+        print(ask)
     else:
         print(f'Failed to retrieve data: {response.status_code}')
+        ask = -1
 
     url = 'https://api.marketdata.app/v1/options/chain/' + ticker + '/'
 
@@ -49,18 +53,24 @@ for ticker in tickers:
     # Making the GET request to the API
     response = requests.get(url, headers=headers)
 
+    print(response)
+
         # Checking if the request was successful
     if response. status_code in (200, 203):
         # Parsing the JSON response
         data = response.json()
         strikes = data['strike']
-        i = 0
-        for strike in strikes:
-            while strike <= ask:
-                i += 1
-        last_strike = strikes[i]
-        print(last_strike)
     else:
         print(f'Failed to retrieve data: {response.status_code}')
+        strikes = -1
+
+    print(ask, strikes)
+    if ask != -1 and strikes != -1:
+        i = 0
+        for strike in strikes:
+            if float(strike) < ask:
+                i += 1
+        last_strike = strikes[i-1]
+        print(last_strike)
 
     time.sleep(0.1)
