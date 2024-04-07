9fea6de963f05baca0d10f37b0e588a3cc20e6b7
Ubuntu <Vadim Avkhimenia>
2024-04-07 07:24:15 +0000

    Experimenting

--- main_yf.py
+++ main_yf.py
@@ -1,2 +1,14 @@
 import yfinance as yf
+from datetime import datetime
+
+date_string1 = '2024-04-12'
+date_object1 = datetime.strptime(date_string1, '%Y-%m-%d')
+
+date_string2 = '2024-04-14'
+date_object2 = datetime.strptime(date_string2, '%Y-%m-%d')
+
+print((date_object2 - date_object1).days)
+
 aapl = yf.Ticker("AAPL")
+
+print(aapl.options)
