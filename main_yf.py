235a2deceaf319fbae9f3b44b80745661837ea47
Ubuntu <Vadim Avkhimenia>
2024-04-07 07:55:56 +0000

    Adding criteria

--- main_yf.py
+++ main_yf.py
@@ -11,4 +11,19 @@ print((date_object2 - date_object1).days)
 
 aapl = yf.Ticker("AAPL")
 
-print(aapl.options)
+print(aapl.info['fiftyTwoWeekHigh'])
+
+# Things to consider
+# difference between options
+# Last price
+# percentage of low price against high
+# ticker
+# difference between option legs
+# 1st call strike
+# 2nd call strike
+# 1 put strike
+# 2nd put strike
+# 1st call price
+# 2nd call price
+# 1 put price
+# 2 put price
